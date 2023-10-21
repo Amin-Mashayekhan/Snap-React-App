923e2952cc4201721965adda333081dac9d7c76f
Amin-Mashayekhan <amin.mashayekhan@gmail.com>
2023-10-21 18:43:50 +0330

    Update Body.tsx

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,22 +1,50 @@
-
-import Container from 'react-bootstrap/esm/Container'
+// import Container from 'react-bootstrap/esm/Container'
+import { Link } from 'react-router-dom'
 import Sidebar from './Sidebar'
-import Stack from 'react-bootstrap/esm/Stack'
+// import Stack from 'react-bootstrap/esm/Stack'
 
 interface BodyProps {
-  sidebar : boolean
-  children: JSX.Element[] | JSX.Element 
+  sidebar: boolean
+  children: JSX.Element[] | JSX.Element
 }
 
 export default function Body({ sidebar, children }: BodyProps) {
   return (
-    <Container >
-      <Stack direction='horizontal'>
-        { sidebar && <Sidebar />}
-        <div className='child-container'>
-          { children }
+    <>
+      {/* Start Home Section */}
+      < div id="home" >
+        {/* Navigation */}
+        {sidebar && <Sidebar />}
+        {/* End Navigation */}
+        {/* Start Landing Page Section */}
+        <div className="landing">
+          <div className="home-wrap">
+            <div className="home-inner" />
+          </div>
         </div>
-      </Stack>
-    </Container>
+        <div className="caption text-center">
+          {
+            !children && (
+              <>
+                <h1>Welcome to Snap</h1>
+                <h3>User</h3>
+                <Link className="btn btn-outline-light btn-lg rounded-1 " to='/login'>signing</Link>
+              </>
+            )
+          }
+          {children}
+        </div>
+      </div>   {/* End Landing Page Section */}
+
+      {/* End Home Section */}
+
+      {/* < Container >
+    <Stack direction='horizontal'>
+      {sidebar && <Sidebar />}
+      <div className='child-container'>
+        {children}
+      </div>
+    </Stack> */}
+    </>
   )
 }
