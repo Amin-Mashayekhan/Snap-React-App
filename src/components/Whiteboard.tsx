835982a1b10e9abb40e944ee40494a439b507452
Amin-Mashayekhan <amin.mashayekhan@gmail.com>
2023-10-21 18:44:15 +0330

    Update Whiteboard.tsx

--- src/components/Whiteboard.tsx
+++ src/components/Whiteboard.tsx
@@ -7,18 +7,18 @@ export default function Whiteboard({ students }: { students:string[] }) {
 
 
   return (
-    <>
-      <h3>Whiteboard</h3>
-      <button
+    <div>
+      <h3 className='pb-1'>Whiteboard</h3>
+      <button className='mb-2 btn btn-primary'
         onClick={() => {
           const studentIndex = Math.floor(Math.random() * students.length);
           setWhiteboardStudent(students[studentIndex]);
           // console.log(whiteboardStudent) state updates are stored until rerender 
         }}
       >
-        Select Student
+        Select Student!
       </button>
       <p>Today's whiteboard was performed by {whiteboardStudent}</p>
-    </>
+    </div>
   );
 }
