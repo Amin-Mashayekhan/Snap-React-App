1f759b723e56e95f0a58dd1138efa3ce2d47877e
Amin-Mashayekhan <amin.mashayekhan@gmail.com>
2023-10-23 05:16:06 +0330

    Update Users.tsx

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,18 +1,24 @@
-import { useState, useEffect } from 'react';
-import { User } from '../types';
+import { useEffect, useState } from 'react';
+
+import { UserDetailsType } from '../types';
+import { useNavigate } from 'react-router-dom';
 
 export default function Users() {
-  const [users, setUsers] = useState<Array<Partial<User>>>([]);
+  const [users, setUsers] = useState<Array<Partial<UserDetailsType>>>([]);
 
   useEffect(() => {
     getUsers();
   }, []);
+  const navigate = useNavigate()
 
   async function getUsers() {
-    const res = await fetch('http://127.0.0.1:5000/user');
+    const res = await fetch('${apiRoot}/user');
     if (res.ok) {
       const data = await res.json();
       setUsers(data);
+    } else if (res.status === 401) {
+      // 401 Unauthorized
+      navigate('/logout')
     } else window.alert('Bad Request');
   }
 
@@ -23,7 +29,7 @@ export default function Users() {
       <h3>Users: </h3>
       {users.length > 0 && (
         <>
-          {users.map((user: Partial<User>) => (
+          {users.map((user: Partial<UserDetailsType>) => (
             <>
               <p key={user.id}>
                 {user.username} <small>{user.email} </small>
