7fc1d52dbd0675d7cae075e52829c637ce7196ec
Amin-Mashayekhan <amin.mashayekhan@gmail.com>
2023-10-23 05:16:13 +0330

    Delete FormPage.tsx

--- src/pages/FormPage.tsx
+++
@@ -1,9 +0,0 @@
-import Body from "../components/Body";
-
-export default function FormPage(props: { children: JSX.Element }) {
-  return (
-    <Body sidebar={false}>
-      { props.children }
-    </Body>
-  )
-}
