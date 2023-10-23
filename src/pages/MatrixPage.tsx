d9ae93be0febf23f99ab60083cc28eecf9e106a9
Amin-Mashayekhan <amin.mashayekhan@gmail.com>
2023-10-23 05:16:16 +0330

    Update MatrixPage.tsx

--- src/pages/MatrixPage.tsx
+++ src/pages/MatrixPage.tsx
@@ -24,11 +24,9 @@ export default function MatrixPage() {
 
   return (
     <Body sidebar>
-<div className="card shadow col-lg-9 col-md-10 col-sm-11 mx-auto">
         <Instructors />
         <Students studentArray={students} />
         <Whiteboard students={students} />
-</div>
     </Body>
   )
 }
