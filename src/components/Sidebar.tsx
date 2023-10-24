a7145ee90e9a4a854ccfae164eb59258a12f6a8f
Amin-Mashayekhan <amin.mashayekhan@gmail.com>
2023-10-24 17:28:21 +0330

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -18,43 +18,18 @@ export default function Sidebar() {
 
   return (
     <>
-      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar w/ text</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-          </ul>
-          <span className="navbar-text">
-            Navbar text with an inline element
-          </span>
-        </div>
-      </nav> */}
       <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
         <div className="navbar-brand">
           <img className='rounded-circle' src={logo} alt='snap taxi' />
         </div>
         {
           user.token && (
-            // <div className="navbar-toggler">
-            //   <span className="navbar-toggler-icon" />
-            // </div>
-            <button onClick={() => setIsNavbarOpen(prevState => !prevState)} className={`navbar-toggler ${!isNavbarOpen && 'collapsed'}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded={`${isNavbarOpen ? "true" : "false"}`} aria-label="Toggle navigation">
+            <button onClick={() => setIsNavbarOpen(prevState => !prevState)} className={`navbar-toggler ${!isNavbarOpen ? 'collapsed' : ''}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded={`${isNavbarOpen ? "true" : "false"}`} aria-label="Toggle navigation">
               <span className="navbar-toggler-icon" />
             </button>
           )
         }
-        <div className={`collapse navbar-collapse ${isNavbarOpen && 'show'}`} id="navbarResponsive">
+        <div className={`collapse navbar-collapse ${isNavbarOpen ? 'show' : ''}`} id="navbarResponsive">
           <ul className="navbar-nav w-100">
             {
               user.token && (
@@ -77,15 +52,13 @@ export default function Sidebar() {
                 </>
               )
             }
-            <li className="nav-item d-flex align-items-end ms-auto">
-              {
-                user.token ? (
+            {
+              user.token && (
+                <li className="nav-item d-flex align-items-end ms-auto">
                   <Link to='/logout' className=" btn btn-outline-light" >Logout</Link>
-                ) : (
-                  <Link to='/login' className=" btn btn-outline-light" >Signing</Link>
-                )
-              }
-            </li>
+                </li>
+              )
+            }
           </ul>
         </div>
       </nav>
