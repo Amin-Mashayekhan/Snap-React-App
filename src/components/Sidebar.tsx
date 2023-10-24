e6b8f4d073718cfa6007c911cf2ca3ec6e85288d
Amin-Mashayekhan <amin.mashayekhan@gmail.com>
2023-10-24 17:18:14 +0330

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@
 import { Link } from 'react-router-dom';
 import { UserContext } from '../contexts/UserProvider';
 import logo from './../assets/images/logo.jpg';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 // import Nav from 'react-bootstrap/esm/Nav'
 // import { Navbar } from 'react-bootstrap'
@@ -14,50 +14,82 @@ import { useContext } from 'react';
 export default function Sidebar() {
 
   const { user } = useContext(UserContext)
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <div className="navbar-brand">
-        <img className='rounded-circle' src={logo} alt='snap taxi' />
-      </div>
-      <div className="navbar-toggler">
-        <span className="navbar-toggler-icon" />
-      </div>
-      <div className="collapse navbar-collapse" id="navbarResponsive">
-        <ul className="navbar-nav w-100">
-          {
-            user.token && (
-              <>
-                <li className="nav-item">
-                  <Link to='/' className="nav-link" >Home</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={`/user/${user.username}`} className="nav-link" >My Page</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/users' className="nav-link" >Other Passengers</Link>
-                </li>
-                {/* <li className="nav-item">
-                  <Link to='/feed' className="nav-link" >Posts</Link>
-                </li> */}
-                <li className="nav-item">
-                  <Link to='/country' className="nav-link" >Country (API)</Link>
-                </li>
-              </>
-            )
-          }
-          <li className="nav-item d-flex align-items-end ms-auto">
+    <>
+      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar w/ text</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+          </ul>
+          <span className="navbar-text">
+            Navbar text with an inline element
+          </span>
+        </div>
+      </nav> */}
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div className="navbar-brand">
+          <img className='rounded-circle' src={logo} alt='snap taxi' />
+        </div>
+        {
+          user.token && (
+            // <div className="navbar-toggler">
+            //   <span className="navbar-toggler-icon" />
+            // </div>
+            <button onClick={() => setIsNavbarOpen(prevState => !prevState)} className={`navbar-toggler ${!isNavbarOpen && 'collapsed'}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded={`${isNavbarOpen ? "true" : "false"}`} aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+            </button>
+          )
+        }
+        <div className={`collapse navbar-collapse ${isNavbarOpen && 'show'}`} id="navbarResponsive">
+          <ul className="navbar-nav w-100">
             {
-              user.token ? (
-                <Link to='/logout' className=" btn btn-outline-light" >Logout</Link>
-              ) : (
-                <Link to='/login' className=" btn btn-outline-light" >Signing</Link>
+              user.token && (
+                <>
+                  <li className="nav-item">
+                    <Link to='/' className="nav-link" >Home</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to={`/user/${user.username}`} className="nav-link" >My Page</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to='/users' className="nav-link" >Other Passengers</Link>
+                  </li>
+                  {/* <li className="nav-item">
+                    <Link to='/feed' className="nav-link" >Posts</Link>
+                  </li> */}
+                  <li className="nav-item">
+                    <Link to='/country' className="nav-link" >Country (API)</Link>
+                  </li>
+                </>
               )
             }
-          </li>
-        </ul>
-      </div>
-    </nav>
+            <li className="nav-item d-flex align-items-end ms-auto">
+              {
+                user.token ? (
+                  <Link to='/logout' className=" btn btn-outline-light" >Logout</Link>
+                ) : (
+                  <Link to='/login' className=" btn btn-outline-light" >Signing</Link>
+                )
+              }
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
     // <Navbar sticky='top' className='flex-column sidebar'>
     //   <Nav.Item>
     //     <Nav.Link as={NavLink} to='/'>Matrix Page</Nav.Link>
