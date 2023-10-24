8abc19d2b4b7e24ded526551ce9241b2c1997cde
Amin-Mashayekhan <amin.mashayekhan@gmail.com>
2023-10-24 16:29:58 +0330

    Update DeleteForm.tsx

--- src/components/forms/DeleteForm.tsx
+++ src/components/forms/DeleteForm.tsx
@@ -23,30 +23,34 @@ export default function DeleteForm() {
 
   async function handleDeleteData(e: FormEvent<HTMLElement>) {
     e.preventDefault()
-    setPageLoading(true)
-    const res = await fetch(`${apiRoot}/passenger`, {
-      method: "DELETE",
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${user.token!}`
-      },
-      body: JSON.stringify({
-        username: usernameField.current!.value,
-        password: passwordField.current!.value
+    if (usernameField.current?.value) {
+      setPageLoading(true)
+      const res = await fetch(`${apiRoot}/passenger`, {
+        method: "DELETE",
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${user.token!}`
+        },
+        body: JSON.stringify({
+          username: usernameField.current!.value,
+          password: passwordField.current!.value
+        })
       })
-    })
-    setPageLoading(false)
-    if (res.ok) {
-      Toast('success', 'Deleting was done successfully.')
-      const data = await res.json()
-      console.log(data)
-      navigate('/logout')
-    } else if (res.status === 401) {
-      // 401 Unauthorized
-      Toast('error', 'For the security of your account, please login again.')
-      navigate('/logout')
+      setPageLoading(false)
+      if (res.ok) {
+        Toast('success', 'Deleting was done successfully.')
+        const data = await res.json()
+        console.log(data)
+        navigate('/logout')
+      } else if (res.status === 401) {
+        // 401 Unauthorized
+        Toast('error', 'For the security of your account, please login again.')
+        navigate('/logout')
+      } else {
+        Toast('error', 'An error occurred, please try again.')
+      }
     } else {
-      Toast('error', 'An error occurred, please try again.')
+      Toast('error', 'Please fill in all of required fields.')
     }
   }
 
@@ -57,7 +61,7 @@ export default function DeleteForm() {
           <InputComponent name='username' type='text' ref={usernameField} required />
         </div>
         <div className="col-md-4">
-          <InputComponent name='password' type='password' ref={passwordField} required />
+          <InputComponent name='password' type='password' ref={passwordField} />
         </div>
 
         <div className="col-12">
