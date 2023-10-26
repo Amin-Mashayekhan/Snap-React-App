93edd32485eda24a7dbbc84bb07978f14d6857ed
Amin-Mashayekhan <amin.mashayekhan@gmail.com>
2023-10-26 05:39:49 +0330

    Update Logout.tsx

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,15 +1,14 @@
 import { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+
 import Spinner from 'react-bootstrap/Spinner'
 import { UserContext } from '../contexts/UserProvider'
-
+import { useNavigate } from 'react-router-dom'
 
 export default function Logout() {
   const { setUser } = useContext(UserContext)
   const navigate = useNavigate()
 
   useEffect(()=>{
-    localStorage.clear()
     setUser({username: '', token: ''})
     navigate('/')
   },[])
